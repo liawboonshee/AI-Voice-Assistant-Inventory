260b3fceec56ebc4c58a54b4a80573233a49a9af
boonshee <42306009+boonshee@users.noreply.github.com>
2026-07-12 11:35:41 +0800

    Update RecordsPage.tsx

--- client/src/inventory/RecordsPage.tsx
+++ client/src/inventory/RecordsPage.tsx
@@ -1,55 +1,154 @@
+import { useEffect, useState } from 'react'
 import { loadRecords } from './Records'
 
 
 export default function RecordsPage(){
 
-const list = loadRecords()
+
+  const [records,setRecords] = useState(loadRecords())
 
 
-return (
+  useEffect(()=>{
 
-<div style={{padding:24}}>
+    const timer=setInterval(()=>{
 
-<h1>📋 交易记录</h1>
+      setRecords(loadRecords())
 
-
-{
-list.map((item,index)=>(
-
-<div key={index}>
-
-<p>
-{item.type==='sale'
-?'📤 出货'
-:'📥 进货'}
-</p>
-
-<p>
-日期：
-{item.date}
-</p>
-
-<p>
-重量：
-{item.weight} g
-</p>
-
-<p>
-金额：
-{item.amount}
-</p>
-
-<hr/>
-
-</div>
-
-))
-
-}
+    },500)
 
 
-</div>
+    return()=>clearInterval(timer)
 
-)
+
+  },[])
+
+
+
+
+  return(
+
+    <div style={{padding:24}}>
+
+
+      <h1>📋 交易记录</h1>
+
+
+
+      {
+        records.length===0 && (
+
+          <p>
+            暂无记录
+          </p>
+
+        )
+      }
+
+
+
+
+      {
+        records.map((item,index)=>(
+
+
+          <div
+
+            key={index}
+
+            style={{
+
+              border:'1px solid #555',
+
+              padding:12,
+
+              marginBottom:10,
+
+              borderRadius:8
+
+            }}
+
+          >
+
+
+
+            <h3>
+
+              {
+                item.type==='sale'
+                ?
+                '📤 出货'
+                :
+                '📥 进货'
+              }
+
+            </h3>
+
+
+
+
+            <p>
+
+              日期：
+
+              {item.date}
+
+            </p>
+
+
+
+
+            {
+              item.customer && (
+
+                <p>
+
+                  客户：
+
+                  {item.customer}
+
+                </p>
+
+              )
+            }
+
+
+
+
+
+            <p>
+
+              重量：
+
+              {item.weight.toFixed(2)} g
+
+            </p>
+
+
+
+
+            <p>
+
+              金额：
+
+              {item.amount.toFixed(2)}
+
+            </p>
+
+
+
+
+          </div>
+
+
+        ))
+
+      }
+
+
+
+    </div>
+
+  )
+
 
 }
