6d7eb01a747031079b9c5c402ad2932e9740041e
boonshee <42306009+boonshee@users.noreply.github.com>
2026-07-12 09:59:20 +0800

    Update Home.tsx

--- client/src/inventory/Home.tsx
+++ client/src/inventory/Home.tsx
@@ -1,11 +1,83 @@
-export default function Home() {
-  return (
-    <div>
-      <h2>🏠 首页</h2>
+import { useEffect, useState } from 'react'
+import { loadInventory } from './Storage'
 
-      <p>今日收入：0</p>
-      <p>总盈利：0</p>
-      <p>当前库存：0.00 g</p>
+
+export default function Home(){
+
+  const [data,setData] = useState(loadInventory())
+
+
+  useEffect(()=>{
+
+    const timer=setInterval(()=>{
+
+      setData(loadInventory())
+
+    },500)
+
+
+    return ()=>clearInterval(timer)
+
+  },[])
+
+
+
+  const avgCost =
+    data.stock > 0
+    ? data.totalWeightCost / data.stock
+    : 0
+
+
+
+  return (
+
+    <div style={{padding:24}}>
+
+
+      <h1>📦 库存宝</h1>
+
+
+      <h2>库存管理系统</h2>
+
+
+      <p>
+        今日收入：
+        {data.income.toFixed(2)}
+      </p>
+
+
+      <p>
+        当前库存：
+        {data.stock.toFixed(2)} g
+      </p>
+
+
+      <p>
+        库存本金：
+        {data.totalWeightCost.toFixed(2)}
+      </p>
+
+
+      <p>
+        平均成本：
+        {avgCost.toFixed(2)} /g
+      </p>
+
+
+      <p>
+        总成本：
+        {data.cost.toFixed(2)}
+      </p>
+
+
+      <p>
+        实际盈利：
+        {data.profit.toFixed(2)}
+      </p>
+
+
     </div>
+
   )
+
 }
